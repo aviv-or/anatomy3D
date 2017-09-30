10027ed0dd0ba052f325db7b992c567e9429c751
Carlos Daccarett <cdaccarett93@gmail.com>
2017-09-30 13:03:05 -0700

    reseting 3d model functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,69 +1,57 @@
 //*******************************************//
 //    container for every other 3d object    //
 //*******************************************//
-var scene = new THREE.Scene();
+var renderer, scene, camera, skull;
+init();
+function init() {
+  scene = new THREE.Scene();
 
-//*******************************************//
-//         Eyes to define point of view.     //
-//*******************************************//
-var camera = new THREE.PerspectiveCamera(
-  75, //field of view
-  window.innerWidth / window.innerHeight, //Aspect Ratio
-  0.1, //Near clipping plane
-  1000 //Far clipping plane
-);
+  //*******************************************//
+  //         Eyes to define point of view.     //
+  //*******************************************//
+  camera = new THREE.PerspectiveCamera(
+    75, //field of view
+    window.innerWidth / window.innerHeight, //Aspect Ratio
+    0.1, //Near clipping plane
+    1000 //Far clipping plane
+  );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-//*******************************************//
-//         Updates Viewport on Resize        //
-//*******************************************//
-window.addEventListener('resize', function() {
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  renderer.setSize(width, height);
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
-});
+  //*******************************************//
+  //         Updates Viewport on Resize        //
+  //*******************************************//
+  window.addEventListener('resize', function() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+  });
 
-// var light = new THREE.AmbientLight(0x404040); // soft white light
-// scene.add(light);
+  //controls (ORBIT CONTROL)
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-//controls (ORBIT CONTROL)
-controls = new THREE.OrbitControls(camera, renderer.domElement);
+  var loader = new THREE.ObjectLoader();
+  // var skull;
 
-// load a resource
-// var mtlLoader = new THREE.MTLLoader();
-// mtlLoader.setPath('models/');
-// mtlLoader.load('beaver-best-exported.mtl', function(materials) {
-//   materials.preload();
-//   var objLoader = new THREE.OBJLoader();
-//   objLoader.setMaterials(materials);
-//   objLoader.setPath('models/');
-//   objLoader.load('beaver-best-exported.obj', function(object) {
-//     scene.add(object);
-//   });
-// });
+  loader.load('models/skull.json', function(object) {
+    skull = object;
+    skull.rotation.x = 20 * Math.PI / 180;
+    skull.rotation.y = 180 * Math.PI / 180;
+    skull.position.y = -2;
 
-var loader = new THREE.ObjectLoader();
-var skull;
+    scene.add(skull);
+  });
 
-loader.load('models/skull.json', function(object) {
-  skull = object;
-  skull.rotation.x = 20 * Math.PI / 180;
-  skull.rotation.y = 180 * Math.PI / 180;
-  skull.position.y = -2;
+  // var axisHelper = new THREE.AxisHelper(5);
+  // scene.add(axisHelper);
 
-  scene.add(skull);
-});
-
-// var axisHelper = new THREE.AxisHelper(5);
-// scene.add(axisHelper);
-
-//camera position
-camera.position.z = 10;
+  //camera position
+  camera.position.z = 10;
+}
 
 animate();
 function animate() {
@@ -125,9 +113,9 @@ function skullPosition(target, x, y, z) {
 
 function skullReset(bone) {
   // var target = { x: skull.rotation.x, y: 2.2, z: 3 };
-  var x = document.getElementById(bone+'circle');
-  var y = document.getElementById(bone+'info');
-  var z = document.getElementById(bone+'line');
+  var x = document.getElementById(bone + 'circle');
+  var y = document.getElementById(bone + 'info');
+  var z = document.getElementById(bone + 'line');
   if (
     x.style.display === 'block' &&
     y.style.display === 'block' &&
@@ -152,4 +140,11 @@ function skullReset(bone) {
   });
 
   tween.start();
+}
+
+function resetcanvas() {
+  var oldcanv = document.getElementsByTagName('canvas');
+  oldcanv[0].remove();
+
+  init();
 }
